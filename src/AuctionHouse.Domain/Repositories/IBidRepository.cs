143c5833535e368cb547e3267774111437a60df1
eduardo <eddytap01@gmail.com>
2026-07-20 15:00:28 -0400

    Add bid repository.

---
+++ src/AuctionHouse.Domain/Repositories/IBidRepository.cs
@@ -0,0 +1,13 @@
+using AuctionHouse.Domain.Entities;
+
+namespace AuctionHouse.Domain.Repositories;
+
+public interface IBidRepository
+{
+    Task<Bid?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Bid>> GetAllAsync(); 
+    Task<IEnumerable<Bid?>> GetByBidderIdAsync(Guid id);
+    Task AddAsync(Bid bid); 
+    Task UpdateAsync(Bid bid);
+    Task DeleteAsync(Guid id); 
+}
