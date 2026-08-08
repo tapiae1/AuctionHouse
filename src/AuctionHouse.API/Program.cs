4d252798decd31b3714dfd9b797d8dbbe0ad7bdc
eduardo <eddytap01@gmail.com>
2026-08-07 21:55:15 -0400

    Register services

--- src/AuctionHouse.API/Program.cs
+++ src/AuctionHouse.API/Program.cs
@@ -1,8 +1,22 @@
+/*Since this is the first time that I worked with ASP .NET Core, there are some concepts that I just don't really get.
+ The biggest one that I had trouble with are the life times to the different services in the container. 
+ So a lot of the time that I had spent working on this file is just understanding the ASP .NET Core services. */
+
+// The lifetime of the User and Bid repositories should be Singleton because they need to persist through multiple requests. 
+// Otherwise, there will be a new database everytime a new request is made. 
+using AuctionHouse.Domain.Repositories;
+using AuctionHouse.Infrastructure.Repositories; 
+using AuctionHouse.Application.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IAuctionRepository, AuctionRepository>();
+builder.Services.AddSingleton<IBidRepository, BidRepository>();
+builder.Services.AddScoped<PlaceBidService>();
 
 var app = builder.Build();
 
@@ -16,3 +30,8 @@ app.UseHttpsRedirection();
 
 
 app.Run();
+
+
+
+
+//Jade-Monet Wiglesworth loves Eduardo Alberto Tapia-Gonzalez very much! <3
