0fe531aa6e0ac1a1a80930cccbbd372ebbb773a9
eduardo <eddytap01@gmail.com>
2026-08-11 14:31:32 -0400

    Add CreateAuctionService

---
+++ src/AuctionHouse.Application/Services/CreateAuctionService.cs
@@ -0,0 +1,27 @@
+using AuctionHouse.Domain.Entities;
+using AuctionHouse.Domain.Exceptions;
+using AuctionHouse.Domain.Repositories;
+
+namespace AuctionHouse.Application.Services;
+
+public class CreateAuctionService
+{
+    private readonly IAuctionRepository _auctionRepository;
+    private readonly IUserRepository _userRepository;// TODO: Add user validation check when creating an auction. 
+
+    public CreateAuctionService(IAuctionRepository auctionRepository, IUserRepository userRepository)
+    {
+        _auctionRepository = auctionRepository;
+        _userRepository = userRepository;
+    }
+
+
+    public async Task<Auction> CreateAuctionAsync(Guid sellerId, string title, string description, decimal startingPrice, DateTime startTime, DateTime endTime)
+    { 
+        // Create auction and Add to repo
+        var auction = Auction.Create(sellerId, title, description, startingPrice, startTime, endTime); 
+        await _auctionRepository.AddAsync(auction); // TODO: what if there's already an auction? 
+        
+        return auction; 
+    }
+}
