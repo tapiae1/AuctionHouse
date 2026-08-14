11def3b8a4d3e96d3772ae20e8a3b19fe888a3a1
eduardo <eddytap01@gmail.com>
2026-08-14 11:27:31 -0400

    Remove GetByUsername method

--- src/AuctionHouse.Domain/Repositories/IUserRepository.cs
+++ src/AuctionHouse.Domain/Repositories/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace AuctionHouse.Domain.Repositories;
 
 public interface IUserRepository 
 {
-    Task<User> GetByUsernameAsync(string username);
+    //Task<User> GetByUsernameAsync(string username);
     Task<User?> GetByIdAsync(Guid id); 
     Task<IEnumerable<User>> GetAllAsync(); 
     Task AddAsync(User user);
